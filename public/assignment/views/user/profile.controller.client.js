2083d0b34a5d6bae1b5a8feca771cf867b3faf1c
swapnilkmahajan <mahajan.sw@husky.neu.edu>
2016-05-28 10:17:31 -0400

    update user functionality added to controller

--- public/assignment/views/user/profile.controller.client.js
+++ public/assignment/views/user/profile.controller.client.js
@@ -6,20 +6,30 @@
         .module("WebAppMaker")
         .controller("ProfileController", ProfileController);
 
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ]
+    
     function ProfileController($routeParams) {
         var vm = this;
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ]
+        vm.updateUser = updateUser;
+        var index = -1;
 
         var id = $routeParams["id"];
         for(var i in users){
             if(users[i]._id === id){
                 vm.user = users[i];
+                index = i;
             }
+        }
+        
+        function updateUser() {
+            users[index].firstName  = vm.user.firstName;
+            users[index].lastName = vm.user.lastName;
+            vm.success = "Success";
         }
     }
 })();
